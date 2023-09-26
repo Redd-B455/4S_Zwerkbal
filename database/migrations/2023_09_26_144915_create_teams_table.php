5f8ad8978d9af1dff6ec3725ac8044033b0787e0
Red Velvet <105586150+Redd-B455@users.noreply.github.com>
2023-09-26 17:17:37 +0200

    Completed H2

--- database/migrations/2023_09_26_144915_create_teams_table.php
+++ database/migrations/2023_09_26_144915_create_teams_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('teams', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('type');
+            $table->text('origin')->nullable($value = true);
             $table->timestamps();
         });
     }
