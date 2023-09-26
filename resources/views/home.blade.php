68bdc1fd7cfe9745f2b73fbaf9bc2d744bb8aa6a
Red Velvet <105586150+Redd-B455@users.noreply.github.com>
2023-09-26 16:31:14 +0200

    Zwerkbal - Almost Done with week 1
    The dashboard needs fixing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,15 +14,17 @@
     <h1>Dashboard</h1>
     <p>Zwerkbal (Engels: Quidditch) is een sport voor tovenaars uit de Harry Potterboeken van J.K. Rowling. In de wereld van Harry Potter is Zwerkbal een populaire sport: veel mensen praten erover mee en zijn zeer enthousiast als er een groot toernooi wordt gespeeld. Ook heeft iedere heks of tovenaar wel een favoriet Zwerkbalteam.</p>
 
-    <div class="FlexContainer">
-        <div class="FlexItem">
-            <p>aantal aankomende toernooien: <br><b>5</b></p>
+    <div class="DashboardContainer">
+        <div class="DashboardItem">
+            <p>Aantal Aankomende Toernooien: <br><b>5</b></p>
         </div>
-        <div class="FlexItem2">
-            <p>aantal aankomende toernooien: <br><b>5</b></p>
-        </div>
-        <div class="FlexItem3">
-            <img src=".\public\img\snitch.png" alt="Missing Snitch">
-        </div>
+        <div class="DashboardItem">
+            <p>Totaal Aantal Teams: <br><b>26</b></p>
+        </div>    
     </div>
+    <!-- <div class="GridContainer2">
+        <div class="GridItem3">
+            <img src="{{ url('img/snitch.png') }}" alt="Missing Snitch">
+        </div>
+    </div> -->
 @endsection
